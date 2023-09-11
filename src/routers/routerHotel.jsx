a838b6c6be53bde8b9693a292c4fc8f316300432
bahe <bahebdg@gmail.com>
2023-09-11 23:31:01 +0700

    save point

--- src/routers/routerHotel.jsx
+++ src/routers/routerHotel.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import LayoutContent from "../Layouts/layout-content.jsx";
+import LayoutLogin from "../Layouts/layout-login.jsx";
 import Dashboard from "../Components/Dashboard.jsx";
 import ProfileHotel from "../Components/Profile-hotel.jsx";
 import Kategori from '../Components/Kategori/Kategori.jsx';
