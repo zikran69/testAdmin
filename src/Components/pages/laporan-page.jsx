05feaaec12a7d61f7891783a7663a7a96dfadbfb
bahe <bahebdg@gmail.com>
2023-09-13 18:55:40 +0700

    save point

--- src/Components/pages/laporan-page.jsx
+++ src/Components/pages/laporan-page.jsx
@@ -3,7 +3,7 @@ export default function LaporanPage(){
         <div className="w-full">
             <main className="bg-primary-gray grow overflow-y-auto h-[calc(100vh-67.33px)]">
                 <h1 className="p-4 font-raleway text-2xl font-semibold">Laporan</h1>
-                <form className="font-roboto px-6 mx-4 border rounded-lg bg-white min-w-[800px] max-lg:px-4">
+                <form className="font-roboto px-6 mx-4 border rounded-lg bg-white max-lg:px-4 overflow-auto">
                     <div className="flex justify-start m-4 grid-flow-row">
                         <button id="#" type="button" className="py-1 px-5 bg-blue-400 rounded-md text-sm text-white hover:bg-hover-blue">
                             <i className="ri-filter-2-fill mr-2"></i>Filter 
@@ -22,7 +22,7 @@ export default function LaporanPage(){
                                 <th className="border border-b-2 border-opacity-20 border-secondary-gray p-4 text-left">Customer</th>
                                 <th className="border border-b-2 border-opacity-20 border-secondary-gray p-4 text-left">Transaksi</th>
                                 <th className="border border-b-2 border-opacity-20 border-secondary-gray p-4 text-left">Detail</th>
-                                <th className="border border-b-2 border-opacity-20 border-secondary-gray p-4 text-left w-[210px]">Opsi</th>
+                                <th className="border border-b-2 border-opacity-20 border-secondary-gray p-4 text-left w-[210px] min-w-[210px]">Opsi</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -95,7 +95,7 @@ export default function LaporanPage(){
                                         </tr>
                                     </table>
                                 </td>
-                                <td className="p-4 border-secondary-gray border border-b-2 border-opacity-20 flex flex-nowrap justify-start">
+                                <td className="p-4 border-secondary-gray border border-b-2 border-opacity-20">
                                     <button type="button" title="detail" className="mr-1 py-1 px-5 bg-green-400 rounded-md hover:bg-hover-green"><i className="ri-search-line text-white"></i></button>
                                     <button type="button" title="hapus" className="mr-1 py-1 px-5 bg-red-400 rounded-md hover:bg-hover-red"><i className="ri-delete-bin-line text-white"></i></button>
                                     <button type="button" title="edit" className="py-1 px-5 bg-yellow-400 rounded-md hover:bg-hover-yellow"><i className="ri-file-edit-line text-white"></i></button>
