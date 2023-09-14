3b847de8295bea443dc4b29b43e19d825537598e
bahe-eman <bahebdg@gmail.com>
2023-09-14 10:05:41 +0700

    save poin

--- src/Components/pages/login-page.jsx
+++ src/Components/pages/login-page.jsx
@@ -24,7 +24,7 @@ export default function LoginPage(){
     }
     
     return(
-        <div className="flex items-center justify-center h-screen bg-[url('./src/assets/loginbg.jpg')] bg-cover">
+        <div className="flex items-center justify-center h-screen bg-[url('./src/assets/login.png')] bg-cover">
             <div className="backdrop-blur-sm bg-white/20 p-6 rounded-xl shadow-xl w-[400px] sm:mx-3">
                 <h1 className="text-3xl font-semibold mb-4 text-center font-roboto">
                     Log In
