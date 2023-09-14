2d882fb9c0610e8dac2d03a316b12609f74668fe
bahe-eman <bahebdg@gmail.com>
2023-09-14 10:36:43 +0700

    save point

--- src/Components/pages/login-page.jsx
+++ src/Components/pages/login-page.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import auth from "../../utils/auth";
+import { useEffect, useState } from "react";
+import bgJson from '../file-bahe/bg-login.json'
 
 export default function LoginPage(){
     const navigate = useNavigate();
@@ -22,39 +24,50 @@ export default function LoginPage(){
             return navigate('/');
         }
     }
+
+    const datas = bgJson;
+    console.log(datas.image);
+    const [bgLogin, setBgLogin] = useState();
+    useEffect(()=>{
+        setBgLogin(datas.image);
+    }, [])
     
-    return(
-        <div className="flex items-center justify-center h-screen bg-[url('./src/assets/login.png')] bg-cover">
-            <div className="backdrop-blur-sm bg-white/20 p-6 rounded-xl shadow-xl w-[400px] sm:mx-3">
-                <h1 className="text-3xl font-semibold mb-4 text-center font-roboto">
-                    Log In
-                </h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-sm font-medium text-gray-700">Username</label>
-                        <input type="text" id="userName" name="username" placeholder="admin" className="mt-1 px-4 py-2 w-full border rounded-md focus:ring focus:ring-indigo-300"/>
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-sm font-medium text-gray-700">Password</label>
-                        <input type="password" id="passWord" name="password" className="mt-1 px-4 py-2 w-full border rounded-md focus:ring focus:ring-indigo-300"/>
-                    </div>
-                    <div className="flex justify-between items-center my-4 p-2">
-                        <div className="flex items-center">
-                            <input type="checkbox" id="remember" />
-                            <label className="ml-2">Remember Me</label>
+    if(bgLogin){
+        return(
+            <div className="flex items-center justify-center h-screen bg-[url('./src/assets/login.jpg')] bg-cover">
+                {
+                    <div className="backdrop-blur-sm bg-white/20 p-6 rounded-xl shadow-xl w-[400px] sm:mx-3">
+                    <h1 className="text-3xl font-semibold mb-4 text-center font-roboto">
+                        Log In
+                    </h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block text-sm font-medium text-gray-700">Username</label>
+                            <input type="text" id="userName" name="username" placeholder="admin" className="mt-1 px-4 py-2 w-full border rounded-md focus:ring focus:ring-indigo-300"/>
                         </div>
-                        <a href="#" className="text-primary-blue font-semibold hover:underline">Forgot Password?</a>
-                    </div>
-                    <button type="submit" className="w-full bg-secondary-blue text-white py-2 rounded-md hover:bg-hover-blue">
-                    Log In
-                    </button>
-                    <div className="my-4 text-center">
-                        <span>Not an admin?
-                            <a href="src/pages/login/register.html"className="text-secondary-blue font-semibold hover:text-hover-blue hover:underline">Sign Up</a>
-                        </span>
-                    </div>
-                </form>
+                        <div className="mb-4">
+                            <label className="block text-sm font-medium text-gray-700">Password</label>
+                            <input type="password" id="passWord" name="password" className="mt-1 px-4 py-2 w-full border rounded-md focus:ring focus:ring-indigo-300"/>
+                        </div>
+                        <div className="flex justify-between items-center my-4 p-2">
+                            <div className="flex items-center">
+                                <input type="checkbox" id="remember" />
+                                <label className="ml-2">Remember Me</label>
+                            </div>
+                            <a href="#" className="text-primary-blue font-semibold hover:underline">Forgot Password?</a>
+                        </div>
+                        <button type="submit" className="w-full bg-secondary-blue text-white py-2 rounded-md hover:bg-hover-blue">
+                        Log In
+                        </button>
+                        <div className="my-4 text-center">
+                            <span>Not an admin?
+                                <a href="src/pages/login/register.html"className="text-secondary-blue font-semibold hover:text-hover-blue hover:underline">Sign Up</a>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+                }
             </div>
-        </div>
-    )
+        )
+    }
 }
