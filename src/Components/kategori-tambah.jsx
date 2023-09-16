65c745d1cf38de0dba42b342a887f70b8ef069c2
bahe <bahebdg@gmail.com>
2023-09-16 22:05:16 +0700

    kategori save point

---
+++ src/Components/kategori-tambah.jsx
@@ -0,0 +1,27 @@
+export default function KategoriTambah({ database, tambah }) {
+  const tambahKategori = () => {
+    const db_kategori = database;
+    const createKategori = (parKategori, parHarga, parFasilitas, parStatus) => {
+      const newKategori = {
+        id: Math.random() * 321,
+        kategori: parKategori,
+        harga: parHarga,
+        fasilitas: parFasilitas,
+        status: parStatus,
+      };
+      db_kategori.push(newKategori);
+    };
+    createKategori(`${Math.random() * 11}`, 0, "ramdom", "missing");
+    tambah(db_kategori);
+  };
+  return (
+    <button
+      onClick={tambahKategori}
+      id="tambah"
+      type="button"
+      className="py-1 px-5 bg-blue-400 rounded-md text-sm text-white hover:bg-hover-blue"
+    >
+      <i className="ri-hotel-bed-line mr-2"></i>Tambah
+    </button>
+  );
+}
--- src/pages/kategori-kamar-page.jsx
+++ src/pages/kategori-kamar-page.jsx
@@ -1,45 +1,50 @@
-import React, { useState } from "react";
-import db_hotel from '../db_hotel.json'
+import { useState } from "react";
+
+import db_hotel from "../db_hotel.json";
 import KategoriTable from "../Components/kategori-table";
+import KategoriTambah from "../Components/kategori-tambah";
+import KategoriSearch from "../Components/kategori-Search";
 
-export default function KategoiKamarPage(props){
-    const [database, setStorage] = useState(db_hotel);
-    // const [postKategori, setPostKategori] = useState(storage);
-    // let dataKategori = storage;
+export default function KategoiKamarPage() {
+  const [database, setStorage] = useState(db_hotel);
+  // const [postKategori, setPostKategori] = useState(storage);
+  // let dataKategori = storage;
 
-    // if(dataKategori.length>1){
-    //     document.querySelector('table').classList.remove('hidden');
-    // }
+  // if(dataKategori.length>1){
+  //     document.querySelector('table').classList.remove('hidden');
+  // }
 
-    const dataTambahKategori = value=>{
-        setStorage(value.map(el=>el));
-        // dataKategori = value;
-        // setPostKategori(value.map(el=>el));
-        // console.log(storage)
-    }
-    
-    const dataMencari = (value)=>{
-        // setPostKategori(value.map(el=>el));
-    }
-    const deleteKategori = (value)=>{
-        setStorage(value.map(el=>el));
-        // dataKategori = value;
-        // setPostKategori(value.map(el=>el))
-        // console.log(storage)
-    }
-    
-    return(
+  const dataTambahKategori = (value) => {
+    setStorage(value.map((el) => el));
+    // dataKategori = value;
+    // setPostKategori(value.map(el=>el));
+    // console.log(storage)
+  };
+
+  const dataMencari = () => {
+    // setPostKategori(value.map(el=>el));
+  };
+  const deleteKategori = (value) => {
+    setStorage(value.map((el) => el));
+    // dataKategori = value;
+    // setPostKategori(value.map(el=>el))
+    // console.log(storage)
+  };
+
+  return (
     <div className="w-full">
-        <div className="bg-primary-gray grow overflow-y-auto h-[calc(100vh-67.33px)]">
-        <h1 className="p-4 font-raleway text-2xl font-semibold">Kategori Kamar</h1>
+      <div className="bg-primary-gray grow overflow-y-auto h-[calc(100vh-67.33px)]">
+        <h1 className="p-4 font-raleway text-2xl font-semibold">
+          Kategori Kamar
+        </h1>
         <form className="font-roboto px-4 mx-4 border rounded-lg bg-white max-md:text-sm overflow-auto">
-            {/* <div className="grid gap-5 place-items-start sm:flex justify-between m-4 ">
-                <Tambah dataKategori={storage} menambahKategori={dataTambahKategori} />
-                <Search dataKategori={storage} mencariKategori={dataMencari} />
-            </div> */}
-            <KategoriTable database={database} menghapus={deleteKategori}/>
+          <div className="grid gap-5 place-items-start sm:flex justify-between m-4 ">
+            <KategoriTambah database={database} tambah={dataTambahKategori} />
+            <KategoriSearch database={database} mencariKategori={dataMencari} />
+          </div>
+          <KategoriTable database={database} menghapus={deleteKategori} />
         </form>
+      </div>
     </div>
-    </div>
-    )
-}   
+  );
+}
