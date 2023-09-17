424339c4687afd2e7c1dbd723fb663ef802e0fc8
bahe <bahebdg@gmail.com>
2023-09-18 01:34:01 +0700

    use mocapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routers";
 import { global } from "./assets/context";
-import db_hotel from "./db_hotel.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [db, setDb] = useState(db_hotel);
+  const [db, setDb] = useState();
+  useEffect(() => {
+    fetch("https://650742c63a38daf4803f53cd.mockapi.io/kamar")
+      .then((res) => res.json())
+      .then(setDb);
+  }, []);
+  console.log(db);
   const store = {
     database: db,
     updateDb: (value) => {
       setDb(value);
     },
   };
-  return (
-    <BrowserRouter>
-      <global.Provider value={store}>
-        <Router />
-      </global.Provider>
-    </BrowserRouter>
-  );
+  if (db) {
+    return (
+      <BrowserRouter>
+        <global.Provider value={store}>
+          <Router />
+        </global.Provider>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
