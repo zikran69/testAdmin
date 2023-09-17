b9646420cab180a5f395bbf27816405d289846cb
bahe <bahebdg@gmail.com>
2023-09-17 17:55:00 +0700

    save ja

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+import "./index.css";
+import AppsTest from "../bahe-testing/apps-test.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppsTest />
+  </React.StrictMode>
+);
